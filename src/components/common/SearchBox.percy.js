a2b990d1e03c3860fbeef24f0e117d33c670a113
Auge <auge.zaveckaite@vinted.com>
2019-10-21 16:07:17 +0300

    add test configuration

---
+++ src/components/common/SearchBox.percy.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-undef */
+import React from "react";
+import SearchBox from "./SearchBox";
+
+percySnapshot("Button", () => {
+  return <SearchBox>My Searchn box</SearchBox>;
+});
