127df114c68dbaabebb7b09fefe030e20192c6e7
Auge <auge.zaveckaite@vinted.com>
2019-10-21 16:19:56 +0300

    change input type

--- src/components/common/searchBox.jsx
+++ src/components/common/searchBox.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const SearchBox = ({ value, onChange }) => {
   return (
     <input
-      type="text"
+      type="password"
       name="query"
       className="form-control my-3"
       placeholder="Search..."
